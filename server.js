2fa9f67fc8fc3111c20c28a27490bdaca25b553c
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2021-06-07 12:11:17 -0400

    Using the env from travis.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+const lastBGPlayed = process.env.LAST_BG_PLAYED;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    const lastBGPlayed = process.env.LAST_BG_PLAYED;
     console.log(`The last boardgame I played is ${lastBGPlayed}`);
     await sleep(5000);
   }
