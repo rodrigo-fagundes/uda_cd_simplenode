9c339287beaf2b2163bff013440f3b9ab26807da
Rodrigo Moreira Fagundes <rmfagundes@gmail.com>
2021-06-07 11:55:14 -0400

    Env exercise.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    const lastBGPlayed = process.env.LAST_BG_PLAYED;
+    console.log(`The last boardgame I played is ${lastBGPlayed}`);
     await sleep(5000);
   }
 }
